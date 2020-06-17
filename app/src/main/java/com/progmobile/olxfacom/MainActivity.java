4fcae5ea11035ae00162f06c4115c0859884e84e
Jean Jaques <jean.jaques@aluno.ufms.br>
2020-06-17 17:31:20 -0400

    Initial commit with configs

---
+++ app/src/main/java/com/progmobile/olxfacom/MainActivity.java
@@ -0,0 +1,14 @@
+package com.progmobile.olxfacom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

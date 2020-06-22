133cb0ea030ae0647dff1fba3d3b1efdb60c7e90
Larissa Fraga <larissa.fra345@gmail.com>
2020-06-22 16:35:44 -0400

    Foi feito a tela de cadastro de produtos

---
+++ app/src/main/java/com/progmobile/olxfacom/activity/CadastrarAnuncioActivity.java
@@ -0,0 +1,16 @@
+package com.progmobile.olxfacom.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.progmobile.olxfacom.R;
+
+public class CadastrarAnuncioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastrar_anuncio);
+    }
+}

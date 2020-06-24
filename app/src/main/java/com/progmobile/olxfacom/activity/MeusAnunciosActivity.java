752ff34dd4bb387221685e6ad1f54764e2e66ea2
Lavínia <lavininhamelo@hotmail.com>
2020-06-24 07:23:30 -0400

    Listagem dos Meus Anúncios

--- app/src/main/java/com/progmobile/olxfacom/activity/MeusAnunciosActivity.java
+++ app/src/main/java/com/progmobile/olxfacom/activity/MeusAnunciosActivity.java
@@ -1,24 +1,52 @@
 package com.progmobile.olxfacom.activity;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 import com.progmobile.olxfacom.R;
+import com.progmobile.olxfacom.adapter.AdapterAnuncio;
+import com.progmobile.olxfacom.helper.ConfiguracaoFirebase;
+import com.progmobile.olxfacom.model.Anuncio;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import dmax.dialog.SpotsDialog;
 
 public class MeusAnunciosActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerAnuncios;
+    private List<Anuncio> anuncios = new ArrayList<>();
+    private AdapterAnuncio adapterAnuncio;
+    private DatabaseReference anuncioUsuarioRef;
+    private AlertDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meus_anuncios);
+
+
+        anuncioUsuarioRef = ConfiguracaoFirebase.getFirebase()
+                .child("meus_anuncios")
+                .child(ConfiguracaoFirebase.getIdUsuario());
+
+        inicializarComponentes();
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -30,5 +58,45 @@ public class MeusAnunciosActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        recyclerAnuncios.setLayoutManager(new LinearLayoutManager(this));
+        recyclerAnuncios.setHasFixedSize(true);
+        adapterAnuncio = new AdapterAnuncio(anuncios, this);
+        recyclerAnuncios.setAdapter(adapterAnuncio);
+        recuperarAnuncios();
+    }
+
+    private void recuperarAnuncios(){
+
+        dialog = new SpotsDialog.Builder()
+                .setContext(this)
+                .setMessage("Carregando seus anúncios")
+                .build();
+        dialog.show();
+
+        anuncioUsuarioRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                anuncios.clear();
+                for(DataSnapshot ds: dataSnapshot.getChildren()){
+                    anuncios.add(ds.getValue(Anuncio.class));
+                }
+                Collections.reverse(anuncios);
+                adapterAnuncio.notifyDataSetChanged();
+
+                dialog.dismiss();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+    }
+
+    public void inicializarComponentes(){
+        recyclerAnuncios = findViewById(R.id.recyclerAnuncios);
     }
 }
